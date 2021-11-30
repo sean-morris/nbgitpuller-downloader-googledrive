82ed8c347e06e240f61a0fa3fcfae41e049453b6
Sean Morris <sean.smorris@berkeley.edu>
2021-11-30 04:18:11 -0800

    Removed reference to TEMP_DOWNLOAD_DIR
    - remove the use of TEMPDOWNLOAD_DIR from
    nbgitpuller and implement the creation of a
    temporary directory via tempfile.
    - cleaned up documentation

--- src/nbgitpuller_downloader_googledrive/googledrive_downloader.py
+++ src/nbgitpuller_downloader_googledrive/googledrive_downloader.py
@@ -1,8 +1,9 @@
 from nbgitpuller.plugin_hook_specs import hookimpl
+from nbgitpuller.plugin_helper import handle_files_helper
 import re
 import asyncio
 import aiohttp
-import nbgitpuller.plugin_helper as ph
+import tempfile
 
 DOWNLOAD_URL = "https://docs.google.com/uc?export=download"
 
@@ -10,10 +11,15 @@ DOWNLOAD_URL = "https://docs.google.com/uc?export=download"
 @hookimpl
 def handle_files(helper_args, query_line_args):
     """
+    This function calls nbgitpuller's handle_files_helper after first determining the
+    file extension(e.g. zip, tar.gz, etc). Google Drive does not use the name of the file to
+    identify the file on the URL so we must download the file first to get the extension from the
+    response, set up a specialized download function and parameters and then pass off handling
+    to nbgitpuller.
     :param dict helper_args: the function, helper_args["progress_func"], that writes messages to
     the progress stream in the browser window and the download_q, helper_args["download_q"] the progress function uses.
     :param dict query_line_args: this includes all the arguments included on the nbgitpuller URL
-    :return two parameter json unzip_dir and origin_repo_path
+    :return two parameter json output_dir and origin_repo_path
     :rtype json object
     """
     loop = asyncio.get_event_loop()
@@ -22,19 +28,22 @@ def handle_files(helper_args, query_line_args):
     response = loop.run_until_complete(get_response_from_drive(DOWNLOAD_URL, get_id(repo)))
     ext = determine_file_extension_from_response(response)
     helper_args["download_q"].put_nowait(f"Archive is: {ext}\n")
-    temp_download_file = f"{ph.TEMP_DOWNLOAD_REPO_DIR}/download.{ext}"
+    temp_download_dir = tempfile.TemporaryDirectory(dir="/tmp")
+    temp_download_file = f"{temp_download_dir.name}/download.{ext}"
 
     helper_args["extension"] = ext
     helper_args["dowload_func"] = download_archive_for_google
     helper_args["dowload_func_params"] = query_line_args, temp_download_file
 
-    tasks = ph.handle_files_helper(helper_args, query_line_args), helper_args["wait_for_sync_progress_queue"]()
+    tasks = handle_files_helper(helper_args, query_line_args), helper_args["wait_for_sync_progress_queue"]()
     result_handle, _ = loop.run_until_complete(asyncio.gather(*tasks))
     return result_handle
 
 
 def get_id(repo):
     """
+    This gets the id of the file from the URL.
+
     :param str repo: the url to the compressed file contained the google id
     :return the google drive id of the file to be downloaded
     :rtype str
@@ -46,13 +55,13 @@ def get_id(repo):
 
 def get_confirm_token(session, url):
     """
+    Google may include a confirm dialog if the file is too big. This retreives the
+    confirmation token and uses it to complete the download.
+
     :param aiohttp.ClientSession session: used to the get the cookies from the reponse
     :param str url : the url is used to filter out the correct cookies from the session
     :return the cookie if found or None if not found
     :rtype str
-
-    This used to determine whether or not Google needs you to confirm a large download
-    file is being downloaded
     """
     cookies = session.cookie_jar.filter_cookies(url)
     for key, cookie in cookies.items():
@@ -63,10 +72,10 @@ def get_confirm_token(session, url):
 
 async def download_archive_for_google(args, temp_download_file):
     """
+    This requests the file from the repo(url) given and saves it to the disk.
+
     :param map args: key-value pairs includes repo path
     :param str temp_download_file: the path to save the requested file to
-
-    This requests the file from the repo(url) given and saves it to the disk
     """
     yield "Downloading archive ...\n"
     try:
@@ -97,16 +106,17 @@ async def download_archive_for_google(args, temp_download_file):
 
 async def get_response_from_drive(url, id):
     """
-    :param str url: the google download URL
-    :param str id: the google id of the file to download
-    :return response object
-    :rtype json object
     You need to check to see that Google Drive has not asked the
     request to confirm that they disabled the virus scan on files that
     are bigger than 100MB(The size is mentioned online but I did not see
     confirmation - something larger essentially). For large files, you have
     to request again but this time putting the 'confirm=XXX' as a query
     parameter.
+
+    :param str url: the google download URL
+    :param str id: the google id of the file to download
+    :return response object
+    :rtype json object
     """
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params={'id': id}) as response:
@@ -120,6 +130,7 @@ async def get_response_from_drive(url, id):
 
 def determine_file_extension_from_response(response):
     """
+    This retrieves the file extension from the response.
     :param str response: the response object from the download
     :return the extension indicating the file compression(e.g. zip, tgz)
     :rtype str
